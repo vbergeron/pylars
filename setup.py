22a71fd0c6e405646593d2086ad741c13b8f4e27
vbe <valentin.bergeron@lesfurets.com>
2020-01-27 15:36:07 +0100

    Changed releases

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+import subprocess
+
+
+def get_head_tag():
+    return subprocess.check_output(
+        ["git", "tag", "--points-at", "HEAD"]
+    ).strip().decode("utf-8")
+
+
+setuptools.setup(name="pylars",
+                 version=get_head_tag(),
+                 packages=setuptools.find_packages(),
+                 python_requires=">=3.6",
+                 install_requires=[
+                     "pandas>=0.25.0",
+                 ])
